24c1d3235579030d2028f553c44f1c8a7936e382
Rudr7_7 <ankitgautam50366@gmail.com>
2025-12-07 10:03:31 +0530

    props added

--- prop_project/UI project/src/App.jsx
+++ prop_project/UI project/src/App.jsx
@@ -1,15 +1,18 @@
-import './App.css'
-import Card from './components/Card.jsx'
+// src/App.jsx
+import React from 'react';
+import './App.css';
+
+import { jobsData } from './data.js';
+import Card from './components/Card.jsx';
 
 function App() {
-
   return (
-    <>
     <div id="parent">
-      <Card />
+      {jobsData.map((job, index) => (
+        <Card key={index} {...job} />
+      ))}
     </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
